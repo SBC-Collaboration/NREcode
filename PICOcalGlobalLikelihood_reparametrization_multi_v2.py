aa690a7ddb0947f26e77920aa6376def9a9c7107
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-02-19 21:18:45 -0800

    Update PICOcalGlobalLikelihood_reparametrization_multi_v2.py

--- PICOcalGlobalLikelihood_reparametrization_multi_v2.py
+++ PICOcalGlobalLikelihood_reparametrization_multi_v2.py
@@ -82,6 +82,7 @@ def prep(topdir_searchlocations):
             break
     # now load the simulation outputs
     simfile_list = [os.path.join(topdir, 'simout.bin')]
+    print(simfile_list)
     #simfile_list = [os.path.join(topdir, exp, 'simout.bin')
     #                for exp in experiment_list] # original
     neutron_sims = [sbc.read_bin(simfile) for simfile in simfile_list]
