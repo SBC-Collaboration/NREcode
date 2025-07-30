60817bb6fa82af8eb532b200a15d40f3e0711408
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-07-30 08:46:15 -0700

    Update XeSBC_GlobalLikelihood_reparametrization_multi_v4.py

--- XeSBC_code/XeSBC_GlobalLikelihood_reparametrization_multi_v4.py
+++ XeSBC_code/XeSBC_GlobalLikelihood_reparametrization_multi_v4.py
@@ -233,7 +233,7 @@ def XeSBCcal_post(theta, whichnuisance=np.ones(n_nuisance, dtype=np.bool)):
         
         # expected number of counts 
         nu = SimulatedCounts_2Dmesh(Epts, eb, i_exp,eb_1sig)
-        print("Error2",Epts, eb, i_exp, eb_1sig)
+        # print("Error2",Epts, eb, i_exp, eb_1sig)
         # nu.shape = neutron_Data[i_exp]['counts'].shape = (m, k)
 
         #calculate chi2, looping over multiplicity
@@ -455,7 +455,7 @@ def SimulatedCounts_2Dmesh(Epts, eb, i_exp, eb_1sig):
     p = EfficiencyInterpolation_2Dmesh(neutron_sims[i_exp]['Er'],
                                 neutron_sims[i_exp]['species'],
                                 ET_eff, Epts)
-    print("node1")
+
     # p.shape = (m, n), where n is number of recoils in sim
 
     bubbles = neutron_sims[i_exp]['hiddenvars'][:, 1:] < p[:, :, np.newaxis]
@@ -474,26 +474,26 @@ def SimulatedCounts_2Dmesh(Epts, eb, i_exp, eb_1sig):
     ev_posts = np.zeros(ev_midposts.size + 2, dtype=np.intp)
     ev_posts[1:-1] = ev_midposts + 1
     ev_posts[-1] = neutron_sims[i_exp]['id'].size
-    print("node2")
+
     bubcount = np.zeros((bubbles.shape[0],
                          bubbles.shape[1] + 1,
                          bubbles.shape[2]),
                         dtype=np.uint8)
     np.cumsum(bubbles, axis=1, out=bubcount[:, 1:, :], dtype=np.uint8)
     nbub = np.diff(bubcount[:, ev_posts, :], axis=1)
-    print("node2.1")
+
     vetoed_bubbles = vetocut[:, np.newaxis] * bubbles
     vetocount = np.zeros((bubbles.shape[0],
                           bubbles.shape[1] + 1,
                           bubbles.shape[2]),
                          dtype=np.uint8)
     np.cumsum(vetoed_bubbles, axis=1, out=vetocount[:, 1:, :], dtype=np.uint8)
-    print("node2.2")
+
     vetoed = np.diff(vetocount[:, ev_posts, :], axis=1) > 0
     # print("node2.3", vetoed,vetoed.shape,"\n", nbub, nbub.shape)
     # nbub[vetoed] = 01835.08
     nbub[vetoed] = 0
-    print("node2.4")
+
     fid_bubbles = fidcut[:, np.newaxis] * bubbles
     fidcount = np.zeros((bubbles.shape[0],
                          bubbles.shape[1] + 1,
@@ -503,7 +503,7 @@ def SimulatedCounts_2Dmesh(Epts, eb, i_exp, eb_1sig):
     infid = np.diff(fidcount[:, ev_posts, :], axis=1) > 0
     singles_out_of_fid = (nbub == 1) * (~infid)
     nbub[singles_out_of_fid] = 0
-    print("node3")
+
     # now we have nbub all set
     # with nbub.shape = (m, i, t) where i is number of neutrons
     
@@ -532,7 +532,7 @@ def SimulatedCounts_2Dmesh(Epts, eb, i_exp, eb_1sig):
                        np.exp(exp_rescale) *
                        neutron_data[i_exp]['exp'][:, np.newaxis] *
                        trials_rescale)
-    print("node4")
+
     # print("nu",nu)
     return nu
 
