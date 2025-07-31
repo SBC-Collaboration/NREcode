b5a93f261d6aa782e8af11029623e40690a640eb
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-07-31 11:38:32 -0700

    Update job_fitnest.sh

--- XeSBC_code/job_fitnest.sh
+++ XeSBC_code/job_fitnest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash                                                                                                                                                    
-#SBATCH --time=1:59:00
+#SBATCH --time=5:59:00
 #SBATCH --array=1
 #SBATCH --account=def-kenclark-ab                                                                                                                                             
 #SBATCH --ntasks-per-node=1
